ff4924b807a89822eac2a7899a2f65d815821693
Jenna Fauconnier <jenna.fauconnier@gmail.com>
2024-07-03 11:12:13 +0200

    Initialize a working backend and docker

---
+++ server/src/components/users/controllers.js
@@ -0,0 +1,26 @@
+const User = require('../../models/User');
+
+const getAll = async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
+};
+
+const createUser = async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.json(newUser);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
+};
+
+module.exports = {
+  getAll,
+  createUser,
+};
